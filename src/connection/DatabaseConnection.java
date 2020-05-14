1ae190ee34d2689aa145829bced7390adf0886e6
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-05-15 00:37:11 +0530

    adding DatabaseConnection.java

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -1,5 +1,31 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    
+    private Connection connection = null;
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public boolean attemptConnection() throws SQLException {
+        boolean isConnected = false;
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school?useLegacyDatetimeCode=false&serverTimezone=UTC","user","");
+        if(connection!=null){
+            isConnected = true;
+        }
+        return isConnected;
+    }
+
+    public boolean closeConnection() throws SQLException {
+        boolean isClosed = false;
+        if(getConnection()!=null){
+            getConnection().close();
+            isClosed = true;
+        }
+        return isClosed;
+    }
 }
