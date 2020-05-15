15b79d068d07750a9471ca84be8455a673cf59b9
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-05-15 23:12:53 +0530

    completing getStudent() method

--- src/connection/Main.java
+++ src/connection/Main.java
@@ -8,13 +8,14 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         DatabaseConnection databaseConnection = new DatabaseConnection();
-        if(databaseConnection.attemptConnection()){
+        if (databaseConnection.attemptConnection()) {
             System.out.println("Connection Created Successfully");
         }
         GetStudents students = new GetStudents();
         students.getStudents(databaseConnection.getConnection());
+        students.getStudent(databaseConnection.getConnection());
 
-        GetFee getFee = new GetFee();
-        getFee.getFee(databaseConnection.getConnection());
+//        GetFee getFee = new GetFee();
+//        getFee.getFee(databaseConnection.getConnection());
     }
 }
--- src/students/GetStudents.java
+++ src/students/GetStudents.java
@@ -3,8 +3,10 @@ package students;
 import com.mysql.cj.protocol.Resultset;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class GetStudents {
+    Scanner scanner = new Scanner(System.in);
     public void getStudents(Connection connection) throws SQLException {
         Statement statement=connection.createStatement();
         ResultSet resultset = statement.executeQuery("SELECT * FROM Students");
@@ -16,8 +18,14 @@ public class GetStudents {
         }
     }
     public void getStudent(Connection connection) throws SQLException {
+        System.out.println("Enter the name of the student");
+        String name = scanner.nextLine();
+
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from feeStructure where name=?");
-
-
+        preparedStatement.setString(1,name);
+        ResultSet resultSet =  preparedStatement.executeQuery();
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("rollNumber")+"  "+resultSet.getString("name")+" "+resultSet.getString("due"));
+        }
     }
 }
