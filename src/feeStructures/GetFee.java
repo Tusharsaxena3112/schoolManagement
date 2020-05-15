3d7759d6e2f17c0411f4a55ea6793788ac06b134
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-05-15 22:54:22 +0530

    formatting in GetFee.java

--- src/feeStructures/GetFee.java
+++ src/feeStructures/GetFee.java
@@ -10,7 +10,9 @@ public class GetFee {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM feeStructure");
         while(resultSet.next()){
-            System.out.println(resultSet.getString("rollNumber"));
+            System.out.print(resultSet.getString("rollNumber")+" ");
+            System.out.print(resultSet.getString("name")+" ");
+            System.out.println(resultSet.getString("due"));
         }
     }
 }
