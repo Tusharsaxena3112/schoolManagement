3452043b00fe2377fb2477d9dfa937be385100bc
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-05-15 23:29:38 +0530

    adding registrations

---
+++ src/registration/Registrations.java
@@ -0,0 +1,12 @@
+package registration;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Registrations {
+    Scanner scanner = new Scanner(System.in);
+    public void register(Connection connection){
+        System.out.println("Enter Student's Name:");
+        String name =
+    }
+}
